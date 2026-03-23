5a0d9e23288c299a6e5ab3cbc7be371465b2dca0
MauroRaya <maurorayabr@gmail.com>
2026-03-23 14:08:25 -0300

    feat(project): create project entity in domain layer

---
+++ src/TaskTracker.Domain/Entities/Project.cs
@@ -0,0 +1,9 @@
+namespace TaskTracker.Domain.Entities;
+
+public class Project
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
+}
