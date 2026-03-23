9f44fbf04f46fbeceb12a3b5a350d9ee144e9b9f
MauroRaya <maurorayabr@gmail.com>
2026-03-23 14:09:15 -0300

    feat(user): create user entity in domain layer

---
+++ src/TaskTracker.Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+namespace TaskTracker.Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
+}
